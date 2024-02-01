a75a319da4cd5bb74ced6b10397909419b36a553
metindokmen <kindmemento@gmail.com>
2024-02-01 16:09:37 +0300

    define user model

---
+++ src/models/user.ts
@@ -0,0 +1,44 @@
+// src/models/user.ts
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database';
+
+class User extends Model {
+    public id!: number;
+    public email!: string;
+    public password!: string;
+    public companyName!: string;
+
+    // Timestamps
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        companyName: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        tableName: 'users',
+        modelName: 'User',
+    }
+);
+
+export { User };
