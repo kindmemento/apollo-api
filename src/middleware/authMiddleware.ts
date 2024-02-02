ecfe066d0b35f9c740c7a4d41c42ac8a17f14eea
metindokmen <kindmemento@gmail.com>
2024-02-02 15:09:03 +0300

    retrieve jwt secret from env variables

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,13 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
+const jwtSecret = process.env.JWT_SECRET as string;
+
 export const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
     }
     try {
-        const decodedToken = jwt.verify(token, 'your_secret_key');
+        const decodedToken = jwt.verify(token, jwtSecret);
         req.userId = (decodedToken as any).userId;
         next();
     } catch (error) {
