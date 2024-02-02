357d155630e4db2313cff8ec3ff914ee51b67f6c
metindokmen <kindmemento@gmail.com>
2024-02-02 15:59:17 +0300

    import and config dotenv

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const jwtSecret = process.env.JWT_SECRET as string;
 
 export const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
