53773e9089f98b07244bb4c5258456ec76b83719
metindokmen <kindmemento@gmail.com>
2024-02-03 14:36:10 +0300

    add indexRoutes to main router index.ts file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import authRoutes from './authRoutes';
+import indexRoutes from './indexRoutes';
 
 const router = express.Router();
 
 // Mount authentication routes
 router.use('/auth', authRoutes);
+router.use('/', indexRoutes);
 
 export default router;
