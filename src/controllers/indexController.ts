63d9387cc6b269d1526a70e04200b04e5cccefbc
metindokmen <kindmemento@gmail.com>
2024-02-05 02:10:53 +0300

    enhance addIndex algorithm to match exact requirements provided in task description

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,29 +1,43 @@
 import { Request, Response } from 'express';
 import { Index } from '../models/index';
 import { Consumption } from "../models/consumption";
+import { Op } from "sequelize";
 
 export const addIndex = async (req: Request, res: Response) => {
 	try {
-		let userId = req.userId || req.body.userId // Attempt to extract userId from request headers first, fallback to request body as a failsafe
-		const { indexDate, indexValue } = req.body;
+			const userId = req.userId || req.body.userId // Attempt to extract userId from request headers first, fallback to request body as a failsafe
+			const { indexDate, indexValue } = req.body;
 
-		// Validate input data
-		if (!userId || !indexDate || !indexValue) {
-			return res.status(400).json({ error: "Missing required fields" })
-		}
+			if (!userId || !indexDate || !indexValue || indexValue < 0) {
+					return res.status(400).json({ error: "Invalid or missing fields in the request body" });
+			}
 
-		// Insert index value into Indexes table
-		await Index.create({ userId: userId, indexDate, indexValue });
+			await Index.create({ userId, indexDate, indexValue });
 
-		// @TODO: Calculate consumption
-		const consumptionValue = 0; // Placeholder for now
+			// Check for previous entries
+			const previousIndex = await Index.findOne({
+					where: {
+							userId,
+							indexDate: {
+									[Op.lt]: indexDate // Find the latest index before the current date
+							}
+					},
+					order: [['indexDate', 'DESC']], // Order by indexDate in descending order to get the latest
+					attributes: ['indexValue'] // Only fetch indexValue
+			});
 
-		await Consumption.create({ userId, consumptionDate: indexDate, consumptionValue })
+			if (!previousIndex) {
+				// If this is the first index record, consumption can/will not be recorded and calculated.
+				return res.status(201).json({ message: "Index added successfully." })
+			}
 
-		return res.status(201).json({ message: "Index added successfully" })
+			const consumptionValue = indexValue - previousIndex.indexValue;
+			await Consumption.create({ userId, consumptionDate: indexDate, consumptionValue });
+
+			return res.status(201).json({ message: "Index and consumption added successfully" });
 	} catch (error) {
-		console.error("Error adding index:", error);
-		return res.status(500).json({ error: "Internal server error" })
+			console.error("Error adding index:", error);
+			return res.status(500).json({ error: "Internal server error" });
 	}
 }
 
