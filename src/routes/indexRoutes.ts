52c23516e7c8efb8819bd87958cfc07d0dc53490
metindokmen <kindmemento@gmail.com>
2024-02-05 15:48:44 +0300

    remove deleteIndex route

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { addIndex, deleteIndex } from "../controllers/indexController";
+import { addIndex } from "../controllers/indexController";
 import { authenticateUser } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 // Index routes
 router.post("/addIndex", authenticateUser, addIndex);
-router.delete("/deleteIndex/:indexId", authenticateUser, deleteIndex);
 
 export default router;
